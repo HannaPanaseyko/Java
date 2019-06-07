5e66006b62e739194d7077c7ea17f5a5c348849c
HannaPanaseyko <48181932+HannaPanaseyko@users.noreply.github.com>
2019-06-07 20:18:52 +0300

    Update Main.java

--- untitled2/src/Lesson2/Main.java
+++ untitled2/src/Lesson2/Main.java
@@ -2,16 +2,8 @@ package HomeWork;
 
 public class Main {
     public static void main(String[] args) {
-
-
         Calculation calculation = new Calculation();
-        //calculation.areYouLucky();
         calculation.time();
-
-
-
     }
-
-
 }
 
