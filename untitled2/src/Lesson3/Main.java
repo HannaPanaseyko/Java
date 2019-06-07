44c902d2a0499d63897e753da2266590ff844889
HannaPanaseyko <48181932+HannaPanaseyko@users.noreply.github.com>
2019-06-07 20:14:31 +0300

    Update Main.java

--- untitled2/src/Lesson3/Main.java
+++ untitled2/src/Lesson3/Main.java
@@ -8,18 +8,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number");
         int number = scanner.nextInt();
-     //   System.out.println("your month is:" + getMonthByNumber(monthNumber));
-
         int[] arr = new int[number];
-      //  System.out.println("size of entered array is" + arr.length );
-
-     //   for(int numberOfCircle = 0; numberOfCircle < arr.length; numberOfCircle++){
-     //       arr[numberOfCircle] = numberOfCircle * 2;
-     //       System.out.println(arr[numberOfCircle]);
-     //   }
-     //   for(int numberOfCircle = 10; numberOfCircle >= 0; numberOfCircle--){
-    //        System.out.println(numberOfCircle);
-     //   }
         int numberSecond = number;
         while (number>5){
             System.out.println("number is biger than 5");
@@ -81,42 +70,7 @@ public class Main {
             }
         }
         return result;
-
     }
- //*   public static void main(String[] args) {
-      //  int firstA = 10;
-        //int secondA = 15;
-       //String result = ternar(250,5) ? "true":"false";
-
-     //   if(firstA>secondA) result = "true";
-     //   System.out.println(result);
-
-     //   String result1 = "";
-      //  if(ternar(250, 5)){
-       //     result1 = "true";
-       // } else {
-          //  result1 = "false";
-     //   }
-      //  System.out.println(result1);
- //   }
- //   public static void ifElse(int first, int second){
-     //   if ( first == second){
-    //*        System.out.println("first is equal than second");
-       // } else  if (first > second) {
-       //     System.out.println("first is bigger than second");
-      //  } else if (first < second){
-      //      System.out.println("first is not bigger than second");
-      //  } else  if (first >= second) {
-      //  System.out.println("first is bigger or equal than second");
-  //  } else if (first <= second){
-      //  System.out.println("first is not bigger or equal than second");
- //   } }
-//public static boolean ternar(int first, int second){
-
-  //      return first>second;
-
-//}
-
     public static int getFactorial(int num){
         int result = 1;
         while (num>0){
